e924bc22d45fbd9304b925bb7e8debb6b7127e55
charlie-svl <charliesmitvl@gmail.com>
2023-09-04 14:27:27 +0200

    debug

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,25 +1,19 @@
-from name_c import name_charlie
-from name_al import N
-from name_an import name_andrey
-from akanksha import akanksha_c
+import charlie
+import name_al
+import name_an 
+import akanksha 
 
 
 def introduction():
     print("This is team MedMatch. We are:")
-    name_charlie()
-    N()
-    name_andrey()
-    akanksha_c()
+    charlie.name_charlie()
+    name_al.N()
+    name_an.name_andrey()
+    akanksha.akanksha_c()
+    return
 
 introduction()
 
-def story():
-    act_1(par_c_1(),)
-    act_2()
-    act_3()
-    return
-
-story()
 
 
 
