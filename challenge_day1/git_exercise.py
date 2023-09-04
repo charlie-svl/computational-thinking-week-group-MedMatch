e02d37f13e2efbb71261ab641a16cb68cecf63c6
charlie-svl <charliesmitvl@gmail.com>
2023-09-04 13:08:10 +0200

    a start on infrastructure

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,17 +1,23 @@
 from name_c import name_charlie
 from name_al import N
 
-def introduction(name_1, name_2, name_3, name_4, name_5, name_6):
-    print("This is team MedMatch. We are:", name_1, name_2, name_3, name_4, name_5, name_6)
-    name_1 = name_charlie()
-    name_2 = N()
-    name_3 =
-    name_4 =
-    name_5 =
-    name_6 =
+def introduction():
+    print("This is team MedMatch. We are:")
+    name_charlie()
+    N()
+    
     return 
 
 introduction()
+
+def story():
+    act_1()
+    act_2()
+    act_3()
+    return
+
+story()
+
 
 
 
