6b3ba824e978840966981dbd22509b2a8bdb8441
hugoo-chang <hugo.chang.anllelini@student.uva.nl>
2023-09-04 18:28:12 +0200

    changed second paragraph

--- challenge_day1/hugo.py
+++ challenge_day1/hugo.py
@@ -16,7 +16,7 @@ def par_h_1(name_1, name_2, name_3, name_4, name_5, name_6):
     return
 
 def par_h_2(name_1, name_2, name_3, name_4, name_5, name_6):
-   paragraph = f"As winter descended upon Willowbrook, the grove in the forest transformed into a serene winter wonderland, blanketed in pristine snow. {name_1}, {name_2}, {name_3}, {name_4}, {name_5}, and {name_6} continued to meet in this magical place, and their hearts grew closer with each passing day. Their art adorned the grove's trees, merging seamlessly with the natural beauty around them. Their world expanded, and love blossomed between the six friends."
+   paragraph = f"Their melodies echoed through Willowbrook's quaint streets, capturing the hearts of its residents. One evening, as the sun painted the sky with hues of gold and lavender, {name_1}, {name_2}, {name_3}, {name_4}, {name_5}, and {name_6} played a hauntingly beautiful melody that resonated with everyone who heard it. Drawn in by their talent, the residents approached them, and as they spoke, they discovered a shared love for the arts and a warm connection that would soon become something more profound."
    print(paragraph)
    return
 
