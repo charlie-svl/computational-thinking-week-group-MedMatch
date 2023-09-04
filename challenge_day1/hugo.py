493af46f546996de565413b7a398679a0818c32c
charlie-svl <charliesmitvl@gmail.com>
2023-09-04 16:06:17 +0200

    changes

--- challenge_day1/hugo.py
+++ challenge_day1/hugo.py
@@ -4,17 +4,13 @@ def name_hugo():
 
 
 def par_h_1(name_1, name_2, name_3, name_4, name_5, name_6):
-   print("As winter descended upon Willowbrook, the grove in the forest transformed into a serene winter wonderland, blanketed in pristine snow. " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + ", and " + name_6 + " continued to meet in this magical place and their hearts grew closer with each passing day. Their art adorned the grove's trees, merging seamlessly with the natural beauty around them. Their world expanded, and love blossomed between the six friends.")
+   print("As winter descended upon Willowbrook, the grove in the forest transformed into a serene winter wonderland, blanketed in pristine snow. " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + ", and " + name_6 + " continued to meet in this magical place, and their hearts grew closer with each passing day. Their art adorned the grove's trees, merging seamlessly with the natural beauty around them. Their world expanded, and love blossomed between the six friends.")
    return 
 
 def par_h_2(name_1, name_2, name_3, name_4, name_5, name_6):
    print("Their melodies echoed through Willowbrook's quaint streets, capturing the hearts of its residents. One evening, as the sun painted the sky with hues of gold and lavender, " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + " and " + name_6 + " played a hauntingly beautiful melody that resonated with everyone who heard it. Drawn in by their talent, the residents approached them, and as they spoke, they discovered a shared love for the arts and a warm connection that would soon become something more profound.")
-def par_h_2(name_1, name_2, name_3, name_4, name_5, name_6):
-   print("Their melodies echoed through Willowbrook's quaint streets, capturing the hearts of its residents. One evening, as the sun painted the sky with hues of gold and lavender, " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + " and " + name_6 + " played a hauntingly beautiful melody that resonated with everyone who heard it. Drawn in by their talent, the residents approached them, and as they spoke, they discovered a shared love for the arts and a warm connection that would soon become something more profound.")
    return 
 
-def par_h_3(name_1, name_2, name_3, name_4, name_5, name_6):
-   print ("In the midst of their newfound happiness, an unforeseen tragedy struck Willowbrook like a relentless storm. A deadly disease descended upon the town, casting a shadow over " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + " and " + name_6 + " and the people they loved. It was a merciless affliction that spared no one, threatening to tear apart the very bonds that had brought solace and joy to their lives."
 def par_h_3(name_1, name_2, name_3, name_4, name_5, name_6):
    print ("In the midst of their newfound happiness, an unforeseen tragedy struck Willowbrook like a relentless storm. A deadly disease descended upon the town, casting a shadow over " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + " and " + name_6 + " and the people they loved. It was a merciless affliction that spared no one, threatening to tear apart the very bonds that had brought solace and joy to their lives."
 )
