e84bd79c89af1bf99fb937b1b75aaf70eb820e4b
Almapian <alfatihiabdulmalik@gmail.com>
2023-09-04 15:16:23 +0200

    small changes

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -2,6 +2,8 @@ import charlie
 import name_al
 import name_an 
 import akanksha 
+import name_mc
+import name_h
 
 
 def introduction():
@@ -10,6 +12,8 @@ def introduction():
     name_al.N()
     name_an.name_andrey()
     akanksha.akanksha_c()
+    name_mc.function()
+    name_h.name_hugo()
     return
 
 introduction()
