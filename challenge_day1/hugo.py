e76b138e0c78ffaa168a8c37f12be119ddb89c29
Andrey Bartashevich <abartashevich@umassd.edu>
2023-09-04 17:04:03 +0200

    updated hugo.py file

--- challenge_day1/hugo.py
+++ challenge_day1/hugo.py
@@ -2,9 +2,6 @@ def name_hugo():
    print("Hugo Chang")
    return 
 
-def name_story():
-   name = "Theo"
-   return name
 
 def par_h_1(name_1, name_2, name_3, name_4, name_5, name_6):
    print("As winter descended upon Willowbrook, the grove in the forest transformed into a serene winter wonderland, blanketed in pristine snow. " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + ", and " + name_6 + " continued to meet in this magical place, and their hearts grew closer with each passing day. Their art adorned the grove's trees, merging seamlessly with the natural beauty around them. Their world expanded, and love blossomed between the six friends.")
@@ -15,5 +12,6 @@ def par_h_2(name_1, name_2, name_3, name_4, name_5, name_6):
    return 
 
 def par_h_3(name_1, name_2, name_3, name_4, name_5, name_6):
-   print ("In the midst of their newfound happiness, an unforeseen tragedy struck Willowbrook like a relentless storm. A deadly disease descended upon the town, casting a shadow over " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + " and " + name_6 + " and the people they loved. It was a merciless affliction that spared no one, threatening to tear apart the very bonds that had brought solace and joy to their lives.")
+   print ("In the midst of their newfound happiness, an unforeseen tragedy struck Willowbrook like a relentless storm. A deadly disease descended upon the town, casting a shadow over " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + " and " + name_6 + " and the people they loved. It was a merciless affliction that spared no one, threatening to tear apart the very bonds that had brought solace and joy to their lives."
+)
    return 
