b9f7aac21a1aaff25f1df74bde524489fabebbc8
hugoo-chang <hugo.chang.anllelini@student.uva.nl>
2023-09-04 15:11:23 +0200

    'nothing'

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,4 +1,27 @@
-from name_c.py import name_charlie
+from name_c import name_charlie
+from name_al import N
+from name_an import name_andrey
+from akanksha import akanksha_c
+
+
+def introduction():
+    print("This is team MedMatch. We are:")
+    name_charlie()
+    N()
+    name_andrey()
+    akanksha_c()
+
+introduction()
+
+def story():
+    act_1(par_c_1(),)
+    act_2()
+    act_3()
+    return
+
+story()
+
+
 
 
 
---
+++ challenge_day1/name_al.py
@@ -0,0 +1,3 @@
+def N():
+    print("Al-fatihi Abdulmalik")
+    return()
