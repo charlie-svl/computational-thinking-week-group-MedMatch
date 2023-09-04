5910f42afeac29245b19a0c4a39b1ffe684c1553
Andrey Bartashevich <abartashevich@umassd.edu>
2023-09-04 18:29:34 +0200

    updated git_exercise updated charlie.py updated hugo.py

--- challenge_day1/charlie.py
+++ challenge_day1/charlie.py
@@ -6,8 +6,6 @@ def name_story():
     name = "Alice"
     return name
 
-name_1=name_story()
-
 
 def par_c_1(name_1, name_2, name_3, name_4, name_5, name_6):
     paragraph = f"This is a story about 6 friends who take the journey through their life together. The {name_1}, {name_2}, and {name_3} have grown up together, as their moms were best friends and decided to have babies at the same time. They met the boys: {name_4}, {name_5}, and {name_6} a couple years later when they entered kindergarten. Together they have a lot of stories to share."
@@ -23,10 +21,3 @@ def par_c_3(name_1, name_2, name_3, name_4, name_5, name_6):
     paragraph = f"{name_3} proclaims this will be her last summer at the country house as her now husband, {name_5}, had passed away a few months back. {name_1} and {name_6} are very understanding. {name_2} and {name_4} were very sad to hear it and started crying."
     print(paragraph)
     return
-
-
-name()
-par_c_1("Alice", "Brianna", "Ella", "Juan", "Igor", "Theo")
-par_c_2("Alice", "Brianna", "Ella", "Juan", "Igor", "Theo")
-par_c_3("Alice", "Brianna", "Ella", "Juan", "Igor", "Theo")
-
--- challenge_day1/hugo.py
+++ challenge_day1/hugo.py
@@ -6,10 +6,6 @@ def name_story():
    name="Theo"
    return name
 
-
-
-
-
 def par_h_1(name_1, name_2, name_3, name_4, name_5, name_6):
     paragraph = f"As winter descended upon Willowbrook, the grove in the forest transformed into a serene winter wonderland, blanketed in pristine snow. {name_1}, {name_2}, {name_3}, {name_4}, {name_5}, and {name_6} continued to meet in this magical place, and their hearts grew closer with each passing day. Their art adorned the grove's trees, merging seamlessly with the natural beauty around them. Their world expanded, and love blossomed between the six friends."
     print(paragraph)
@@ -24,8 +20,3 @@ def par_h_3(name_1, name_2, name_3, name_4, name_5, name_6):
    paragraph = f"In the midst of their newfound happiness, an unforeseen tragedy struck Willowbrook like a relentless storm. A deadly disease descended upon the town, casting a shadow over {name_1}, {name_2}, {name_3}, {name_4}, {name_5}, and {name_6} and the people they loved. It was a merciless affliction that spared no one, threatening to tear apart the very bonds that had brought solace and joy to their lives."
    print(paragraph)
    return
-
-name_hugo()
-par_h_1("Alice", "Brianna", "Ella", "Juan", "Igor", "Theo")
-par_h_2("Alice", "Brianna", "Ella", "Juan", "Igor", "Theo")
-par_h_3("Alice", "Brianna", "Ella", "Juan", "Igor", "Theo")
