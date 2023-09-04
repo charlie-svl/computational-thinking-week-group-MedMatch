3d8e5ef31f1b492139cb7f979044bea7a3517ddd
charlie-svl <charliesmitvl@gmail.com>
2023-09-04 16:15:39 +0200

    fixed a mistake where some parts were deleted

--- challenge_day1/akanksha.py
+++ challenge_day1/akanksha.py
@@ -1,3 +1,22 @@
 def akanksha_c():
     print ("Akanksha Chatterjee")
     return
+
+def name_ac():
+    return "Brianna"
+
+def par_ac_1(name_1, name_2, name_3, name_4, name_5, name_6):
+    print(f"One sunny morning, {name_1}, {name_2}, {name_3}, {name_4}, {name_5}, and {name_6} were exploring the attic of {name_3} house. There they stumbled upon a tattered treasure map. Excitedly, they embarked on a quest to find the hidden treasure. Their journey led them through dense forests, across an old bridge, and into a mysterious cave. Inside the cave, they saw bats and heard eerie echoes, but their determination never wavered. The map eventually led them to a secret beach. After digging with all their might, they unearthed an old chest filled with sparkling gems and ancient artifacts. Their adventure brought them closer than ever, and they cherished the memories of their epic childhood treasure hunt forever.")
+
+    return
+
+def par_ac_2(name_1, name_2, name_3, name_4, name_5, name_6):
+    print(f"{name_1}, {name_2}, {name_3}, were enjoying a cold winter day at their university campus after their lecture. {name_4}, {name_5}, {name_6}, joined them too after a bit. They started with a friendly game of frisbee, laughing as they chased the flying disc. Afterward, they decided to have a picnic near the campus pond, sharing sandwiches and stories from their summer vacations. The soothing sound of the water and the warmth of the sun made it a perfect setting. They couldn't resist the temptation to explore the campus art gallery next, marveling at the creativity of their fellow students. Later, they headed to the university's outdoor amphitheater, where a talented student band was performing. Dancing and cheering, they created unforgettable memories of their university days. As the sun dipped below the horizon, they strolled back to their dorms, grateful for a day filled with laughter, friendship, and the joy of being young and carefree.")
+
+    return
+
+def par_ac_3(name_1, name_2, name_3, name_4, name_5, name_6):
+    print(f"All 6 friends decided to meet up by the park they used to have picnics in when they were students. {name_3} was there the earliest and waited till {name_5} and {name_6} arrived. They found a spot under a tree and set out their mat. {name_1}, {name_2}, and {name_4} arrived soon. Together they enjoyed their sandwiches and talked about life. They also talked about how they found the treasure chest on the island and how they used that money to fund their university studies.")
+
+
+    return
