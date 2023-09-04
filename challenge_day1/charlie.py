d273b785097befe05bfe104649f66caf21da269c
charlie-svl <charliesmitvl@gmail.com>
2023-09-04 15:30:21 +0200

    finalized my paragraphs

--- challenge_day1/charlie.py
+++ challenge_day1/charlie.py
@@ -6,18 +6,16 @@ def name_story():
     name = "Alice"
     return name
 
-def par_c_1(name_1):
+def par_c_1(name_1, name_2, name_3, name_4, name_5, name_6):
     print("This is a story about 6 friends who take the journey through their life together. The", name_1, ",", name_2, "and", name_3, "have grown up together, as their moms were best friends and decided to have babies at the same time. They met the boys: ", name_4, ",", name_5, "and", name_6,  " a couple years later when they entered kindergarten. Togethe r they have a lot of stories to share.")
     return 
 
 
-par_c_1(name_story())
+def par_c_2(name_1, name_2, name_3, name_4, name_5, name_6):
+    print("After ", name_4, ",", name_5, "and", name_6, "started listening to some controversial podcasts, mainly about women," , name_3, ",", name_2 , "and", name_1 , "decided to hold an intervention. They saw the path they were going on and were just in time before their brain was permanently altered." )
+    return
 
-#def par_c_2():
-#    text = ""
-#    return text
-
-#def par_c_3():
-#    text = ""
-#    return text
+def par_c_2(name_1, name_2, name_3, name_4, name_5, name_6):
+    print(name_3, "proclaims this will be her last summer at the country house as her now husband, ", name_5 , "had passed away a few months back." ,name_1, "and" ,name_6, "are very understanding. " , name_2 , "and", name_4, "were very sad to hear it and started crying.")
+    return
 
