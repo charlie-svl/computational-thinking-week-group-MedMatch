2f4ea5d0a4c846c7ce453c20ba244ea4ab0821a1
charlie-svl <charliesmitvl@gmail.com>
2023-09-04 15:58:56 +0200

    story compiling

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -19,6 +19,10 @@ def introduction():
     max.function()
     return
 
+def story(name_1, name_2, name_3, name_4, name_5, name_6):
+    act_1 = charlie.par_c_1(name_1, name_2, name_3, name_4, name_5, name_6), al.par_al_1(name_1, name_2, name_3, name_4, name_5, name_6), akanksha.par_ac_1(name_1, name_2, name_3, name_4, name_5, name_6), andrey.par_an_1(name_1, name_2, name_3, name_4, name_5, name_6), hugo.par_h_1(name_1, name_2, name_3, name_4, name_5, name_6), max.para_1(name_1, name_2, name_3, name_4, name_5, name_6)
+    act_2 = charlie.par_c_2(name_1, name_2, name_3, name_4, name_5, name_6), al.par_al_2(name_1, name_2, name_3, name_4, name_5, name_6), akanksha.par_ac_2(name_1, name_2, name_3, name_4, name_5, name_6), andrey.par_an_2(name_1, name_2, name_3, name_4, name_5, name_6), hugo.par_h_2(name_1, name_2, name_3, name_4, name_5, name_6), max.para_2(name_1, name_2, name_3, name_4, name_5, name_6)
+    act_3 = charlie.par_c_3(name_1, name_2, name_3, name_4, name_5, name_6), al.par_al_3(name_1, name_2, name_3, name_4, name_5, name_6), akanksha.par_ac_3(name_1, name_2, name_3, name_4, name_5, name_6), andrey.par_an_3(name_1, name_2, name_3, name_4, name_5, name_6), hugo.par_h_3(name_1, name_2, name_3, name_4, name_5, name_6), max.para_3(name_1, name_2, name_3, name_4, name_5, name_6)
+    return 
 
-
-
+story(charlie.name_story, al.name_story, akanksha.name_story, andrey.name_story, hugo.name_story, max.name_story)
