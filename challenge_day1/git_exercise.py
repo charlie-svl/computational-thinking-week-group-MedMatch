f204c480d003098344247bfd9268f28077f40bd4
charlie-svl <charliesmitvl@gmail.com>
2023-09-04 15:38:24 +0200

    file changes

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,13 +1,17 @@
 
 # Import functions from different modules
-from name_al import N
-from name_an import name_andrey
-from name_ac import akanksha_c
+import charlie
+import andrey
+import akanksha
+import al
+import hugo
+import max
+
 
 # Define an introduction function
 def introduction():
     print("This is team MedMatch. We are:")
-    N()  # Call the function from name_al module
+    N()  # C
     name_andrey()  # Call the function from name_an module
     akanksha_c()  # Call the function from name_ac module
 
@@ -16,7 +20,5 @@ name_1 = name_ac()
 introduction()
 
 #import charlie
-import name_al
-import name_an 
-import akanksha 
+
 
