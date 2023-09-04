2c55b3cb20bf2f9e21c0a0740e54327b520b4b77
ac2325 <akanksha.chatterjee23@gmail.com>
2023-09-04 20:57:02 +0200

    commit syntax error fix

--- challenge_day1/hugo.py
+++ challenge_day1/hugo.py
@@ -7,16 +7,16 @@ def name_story():
    return name
 
 def par_h_1(name_1, name_2, name_3, name_4, name_5, name_6):
-    paragraph = f"As winter descended upon Willowbrook, the grove in the forest transformed into a serene winter wonderland, blanketed in pristine snow. {name_1}, {name_2}, {name_3}, {name_4}, {name_5}, and {name_6} continued to meet in this magical place, and their hearts grew closer with each passing day. Their art adorned the grove's trees, merging seamlessly with the natural beauty around them. Their world expanded, and love blossomed between the six friends."
+    paragraph = "As winter descended upon Willowbrook, the grove in the forest transformed into a serene winter wonderland, blanketed in pristine snow. " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + ", and " + name_6 + " continued to meet in this magical place, and their hearts grew closer with each passing day. Their art adorned the grove's trees, merging seamlessly with the natural beauty around them. Their world expanded, and love blossomed between the six friends."
     print(paragraph)
     return
 
 def par_h_2(name_1, name_2, name_3, name_4, name_5, name_6):
-   paragraph = f"Their melodies echoed through Willowbrook's quaint streets, capturing the hearts of its residents. One evening, as the sun painted the sky with hues of gold and lavender, {name_1}, {name_2}, {name_3}, {name_4}, {name_5}, and {name_6} played a hauntingly beautiful melody that resonated with everyone who heard it. Drawn in by their talent, the residents approached them, and as they spoke, they discovered a shared love for the arts and a warm connection that would soon become something more profound."
-   print(paragraph)
-   return
+    paragraph = "Their melodies echoed through Willowbrook's quaint streets, capturing the hearts of its residents. One evening, as the sun painted the sky with hues of gold and lavender, " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + ", and " + name_6 + " played a hauntingly beautiful melody that resonated with everyone who heard it. Drawn in by their talent, the residents approached them, and as they spoke, they discovered a shared love for the arts and a warm connection that would soon become something more profound."
+    print(paragraph)
+    return
 
 def par_h_3(name_1, name_2, name_3, name_4, name_5, name_6):
-   paragraph = f"In the midst of their newfound happiness, an unforeseen tragedy struck Willowbrook like a relentless storm. A deadly disease descended upon the town, casting a shadow over {name_1}, {name_2}, {name_3}, {name_4}, {name_5}, and {name_6} and the people they loved. It was a merciless affliction that spared no one, threatening to tear apart the very bonds that had brought solace and joy to their lives."
-   print(paragraph)
-   return
+    paragraph = "In the midst of their newfound happiness, an unforeseen tragedy struck Willowbrook like a relentless storm. A deadly disease descended upon the town, casting a shadow over " + name_1 + ", " + name_2 + ", " + name_3 + ", " + name_4 + ", " + name_5 + ", and " + name_6 + " and the people they loved. It was a merciless affliction that spared no one, threatening to tear apart the very bonds that had brought solace and joy to their lives."
+    print(paragraph)
+    return
