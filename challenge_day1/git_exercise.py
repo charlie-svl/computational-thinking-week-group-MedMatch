3ab5e964ea608b58b58318747e4097a10e8c6935
ac2325 <akanksha.chatterjee23@gmail.com>
2023-09-05 11:32:47 +0200

    fixed story format

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -21,8 +21,11 @@ def introduction():
 
 
 def story(name_1, name_2, name_3, name_4, name_5, name_6):
+    print("Act 1:")
     act_1 = charlie.par_c_1(name_1, name_2, name_3, name_4, name_5, name_6), al.par_al_1(name_1, name_2, name_3, name_4, name_5, name_6), akanksha.par_ac_1(name_1, name_2, name_3, name_4, name_5, name_6), andrey.par_an_1(name_1, name_2, name_3, name_4, name_5, name_6), hugo.par_h_1(name_1, name_2, name_3, name_4, name_5, name_6), maxim.par_m_1(name_1, name_2, name_3, name_4, name_5, name_6)
+    print("Act 2:")
     act_2 = charlie.par_c_2(name_1, name_2, name_3, name_4, name_5, name_6), al.par_al_2(name_1, name_2, name_3, name_4, name_5, name_6), akanksha.par_ac_2(name_1, name_2, name_3, name_4, name_5, name_6), andrey.par_an_2(name_1, name_2, name_3, name_4, name_5, name_6), hugo.par_h_2(name_1, name_2, name_3, name_4, name_5, name_6), maxim.par_m_2(name_1, name_2, name_3, name_4, name_5, name_6)
+    print("Act 3:")
     act_3 = charlie.par_c_3(name_1, name_2, name_3, name_4, name_5, name_6), al.par_al_3(name_1, name_2, name_3, name_4, name_5, name_6), akanksha.par_ac_3(name_1, name_2, name_3, name_4, name_5, name_6), andrey.par_an_3(name_1, name_2, name_3, name_4, name_5, name_6), hugo.par_h_3(name_1, name_2, name_3, name_4, name_5, name_6), maxim.par_m_3(name_1, name_2, name_3, name_4, name_5, name_6)
     return
 
