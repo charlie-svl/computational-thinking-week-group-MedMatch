fe4b6d4cc39c2dad4d58d9086b05af159f30b5ea
charlie-svl <charliesmitvl@gmail.com>
2023-09-06 12:21:55 +0200

    function 1

--- challenge_day3/station1.py
+++ challenge_day3/station1.py
@@ -1,2 +1,9 @@
-def solution_station_1():
-    return
+def solution_station_1(n): 
+
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return solution_station_1(n-1)+solution_station_1(n-2)
+
