3ceefad19707b4211d01d06486f06be75b81190a
Andrey Bartashevich <abartashevich@umassd.edu>
2023-09-06 12:40:17 +0200

    station7 updated

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -1,4 +1,4 @@
-def solution_station_7():
+def solution_station_7(user_input):
     a = 3
     b = -1
     c = 4
@@ -6,7 +6,6 @@ def solution_station_7():
     e = 0.5
     
     variables = {'a': a, 'b': b, 'c': c, 'd': d, 'e': e}
-    user_input = station7_input()
     
     result = eval(user_input, variables)
     return result
