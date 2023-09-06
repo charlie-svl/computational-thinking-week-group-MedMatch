450ec02b6fe16c3c0e13c36afc303a30d7899446
Andrey Bartashevich <abartashevich@umassd.edu>
2023-09-06 13:58:59 +0200

    station6 updated

--- challenge_day3/station6.py
+++ challenge_day3/station6.py
@@ -1,16 +1,4 @@
 import math
 
-<<<<<<< HEAD
-def solution_station_6(x):
-
-    return math.sin(x)
-
-
-for x in range(100):
-    y = solution_station_6(x)
-    print (f"For {x}, you get {y}")
-
-=======
 def solution_station_6(input):
     return math.sin(input)
->>>>>>> 5acb6500c3220644cb80b0ebdcaf0caabde40698
