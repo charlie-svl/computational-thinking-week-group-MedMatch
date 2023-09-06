cf0b8036c9f5587f3cb2c1832d5ce5743f6ced1f
ac2325 <akanksha.chatterjee23@gmail.com>
2023-09-06 12:54:26 +0200

    calendar function updated

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -1,7 +1,12 @@
-def solution_station_7():
+def solution_station_7(user_input):
     a = 3
     b = -1
     c = 4
     d = 7
     e = 0.5
-    return a,b,c,d,e
+    
+    variables = {'a': a, 'b': b, 'c': c, 'd': d, 'e': e}
+    
+    result = eval(user_input, variables)
+    return result
+    
