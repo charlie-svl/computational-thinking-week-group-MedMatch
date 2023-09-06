b20df18387ae163b2a6931b2236c95522d7c3bf3
ac2325 <akanksha.chatterjee23@gmail.com>
2023-09-06 12:59:55 +0200

    station 1 shifted to station 2 (wrong place)

--- challenge_day3/station1.py
+++ challenge_day3/station1.py
@@ -1,25 +1,3 @@
-
-import datetime
-def solution_station_1(input):
-        
-        year, month, day = map(int, input.split('-'))
-
-        date_obj = datetime.datetime(day, month, year)
-        day_of_week = date_obj.weekday()
-
-        weekdays = {
-        0: "Monday",
-        1: "Tuesday",
-        2: "Wednesday",
-        3: "Thursday",
-        4: "Friday",
-        5: "Saturday",
-        6: "Sunday"
-        }
-        
-        weekday = weekdays[day_of_week]
-        print("The day of the week for", input, "is", weekday)
-        return
-
-solution_station_1("17-04-2023")
+def solution_station_6():
+    return
 
--- challenge_day3/station2.py
+++ challenge_day3/station2.py
@@ -1,3 +1,24 @@
-def solution_station_2():
-    
-    return
+
+import datetime
+def solution_station_2(input):
+        
+        year, month, day = map(int, input.split('-'))
+
+        date_obj = datetime.datetime(day, month, year)
+        day_of_week = date_obj.weekday()
+
+        weekdays = {
+        0: "月曜日",
+        1: "火曜日",
+        2: "水曜日",
+        3: "木曜日",
+        4: "金曜日",
+        5: "土曜日",
+        6: "日曜日"
+        }
+        
+        weekday = weekdays[day_of_week]
+        print (weekday)
+        return weekday
+
+solution_station_2("04-04-2023")
